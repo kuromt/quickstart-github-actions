ea95f6b9eb4a814798e100f32588c1275b151d10
kuromt <n.kuromatsu@gmail.com>
2022-10-09 20:09:17 +0900

    fix bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ NBDIFF_WEB_EXPORTER_OPTIONS=$4
 EXPORT_DIR="./artifacts"
 
 mkdir $EXPORT_DIR
-git diff $BASE $REMOTE --name-only| grep 'ipynb$'|xargs -L 1 nbdiff-web-exporter --port $PORT --export-dir $EXPORT_DIR $BASE $REMOTE 
+git diff $BASE $REMOTE --name-only| grep '.ipynb$'|xargs -L 1 nbdiff-web-exporter --port $PORT --export-dir $EXPORT_DIR $BASE $REMOTE 
