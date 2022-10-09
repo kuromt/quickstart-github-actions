4b20c682a65bb3b9cd55075296125fdf6d499e24
kuromt <n.kuromatsu@gmail.com>
2022-10-09 21:41:34 +0900

    set export-dir as an outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,9 @@ else
 fi
 REMOTE=$2
 PORT=$3
-NBDIFF_WEB_EXPORTER_OPTIONS=$4
+EXPORT_DIR=$4
+NBDIFF_WEB_EXPORTER_OPTIONS=$5
 
-EXPORT_DIR="./artifacts"
-
-mkdir $EXPORT_DIR
+mkdir -p $EXPORT_DIR
 git diff $BASE $REMOTE --name-only| grep '.ipynb$'|xargs -L 1 nbdiff-web-exporter --port $PORT --export-dir $EXPORT_DIR $BASE $REMOTE 
+echo "::set-output name=export_dir::$EXPORT_DIR"
